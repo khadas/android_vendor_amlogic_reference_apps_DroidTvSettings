7515ef3c08faa686ecb356856b4816a0da8b6dc9
yu fang <yu.fang@amlogic.com>
2019-01-15 18:34:53 +0800

    DroidTvSettings: Unregister BroadcastReceiver in onStop. [1/1]
    PD#SWPL-4204
    
    Problem:
    Memory of DroidTvSettings leaks.
    
    Solution:
    Unregister BroadcastReceiver in onStop.
    
    Verify:
    Verified on all X301/P321.
    
    Change-Id: I6766edde8ff97a710f3a42c21bbbd3e034f25665
    Signed-off-by: yu fang <yu.fang@amlogic.com>

--- src/com/droidlogic/tv/settings/MainFragment.java
+++ src/com/droidlogic/tv/settings/MainFragment.java
@@ -226,6 +226,7 @@ public class MainFragment extends LeanbackPreferenceFragment {
 
     @Override
     public void onStop() {
+        getActivity().getApplicationContext().unregisterReceiver(esnReceiver);
         super.onStop();
     }
 
