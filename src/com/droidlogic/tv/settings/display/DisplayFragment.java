d15857f83806885ddce42307e8a3054ed80506d8
shuai.zhang <shuai.zhang@amlogic.com>
2021-05-14 11:18:58 +0800

    DroidTvSettings: Add memc on off function switch [1/2]
    PD#SWPL-49931
    
    Problem:
    Add memc on off function switch
    
    Solution:
    Add memc on off function switch
    
    Verify:
    T3
    
    Signed-off-by: shuai.zhang<shuai.zhang@amlogic.com>
    Change-Id: I807d39a2e87f6444a3e4dc679bbfb0da1f924390

--- src/com/droidlogic/tv/settings/display/DisplayFragment.java
+++ src/com/droidlogic/tv/settings/display/DisplayFragment.java
@@ -21,54 +21,96 @@ import android.os.Handler;
 import android.support.v17.preference.LeanbackPreferenceFragment;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.ListPreference;
+import android.text.TextUtils;
+import android.util.Log;
+
 
 import android.os.SystemProperties;
 import com.droidlogic.tv.settings.util.DroidUtils;
 import com.droidlogic.tv.settings.SettingsConstant;
 import com.droidlogic.tv.settings.R;
 
-public class DisplayFragment extends LeanbackPreferenceFragment {
+import com.droidlogic.app.SystemControlManager;
 
-	private static final String TAG = "DisplayFragment";
+public class DisplayFragment extends LeanbackPreferenceFragment implements Preference.OnPreferenceChangeListener {
 
-	private static final String KEY_POSITION = "position";
-	private static final String KEY_OUTPUTMODE = "outputmode";
-	private static final String KEY_HDR = "hdr";
-	private static final String KEY_SDR = "sdr";
-	private static final String KEY_DOLBY_VISION    = "dolby_vision";
+    private static final String TAG = "DisplayFragment";
 
-	private boolean mTvUiMode;
+    private static final String KEY_POSITION        = "position";
+    private static final String KEY_OUTPUTMODE      = "outputmode";
+    private static final String KEY_HDR             = "hdr";
+    private static final String KEY_SDR             = "sdr";
+    private static final String KEY_DOLBY_VISION    = "dolby_vision";
+    private static final String KEY_MEMC            = "memc";
+    private static final String PROP_MEMC           = "persist.vendor.sys.memc";
+    private static final int MEMC_OFF               = 0;
+    private static final int MEMC_ON                = 1;
 
-	public static DisplayFragment newInstance() {
-		return new DisplayFragment();
-	}
+    private boolean mTvUiMode;
+    private ListPreference mMEMCPref;
+    private SystemControlManager mSystemControlManager;
+    private static int memcStatus = 0;
+    private static boolean isT3Device = false;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-	}
+    public static DisplayFragment newInstance() {
+        return new DisplayFragment();
+    }
 
-	@Override
-	public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-		setPreferencesFromResource(R.xml.display, null);
-		mTvUiMode = DroidUtils.hasTvUiMode();
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
-		boolean tvFlag = SettingsConstant.needDroidlogicTvFeature(getContext())
-			&& (SystemProperties.getBoolean("tv.soc.as.mbox", false) == false);
-		final Preference outputmodePref = findPreference(KEY_OUTPUTMODE);
-		outputmodePref.setVisible(SettingsConstant.needScreenResolutionFeture(getContext()) && !tvFlag);
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.display, null);
+        mTvUiMode = DroidUtils.hasTvUiMode();
 
-		final Preference screenPositionPref = findPreference(KEY_POSITION);
-		screenPositionPref.setVisible(!tvFlag);
+        boolean tvFlag = SettingsConstant.needDroidlogicTvFeature(getContext())
+            && (SystemProperties.getBoolean("tv.soc.as.mbox", false) == false);
+        final Preference outputmodePref = findPreference(KEY_OUTPUTMODE);
+        outputmodePref.setVisible(SettingsConstant.needScreenResolutionFeture(getContext()) && !tvFlag);
 
-		final Preference sdrPref = findPreference(KEY_SDR);
-		sdrPref.setVisible(false);
+        final Preference screenPositionPref = findPreference(KEY_POSITION);
+        screenPositionPref.setVisible(!tvFlag);
 
-		final Preference hdrPref = findPreference(KEY_HDR);
-		hdrPref.setVisible(false);
+        final Preference sdrPref = findPreference(KEY_SDR);
+        sdrPref.setVisible(false);
 
-		final Preference dvPref =(Preference) findPreference(KEY_DOLBY_VISION);
-		dvPref.setVisible((SystemProperties.getBoolean("vendor.system.support.dolbyvision", false) == true)
-			&& tvFlag);
-	}
+        final Preference hdrPref = findPreference(KEY_HDR);
+        hdrPref.setVisible(false);
+
+        final Preference dvPref =(Preference) findPreference(KEY_DOLBY_VISION);
+        dvPref.setVisible((SystemProperties.getBoolean("vendor.system.support.dolbyvision", false) == true)
+            && tvFlag);
+
+        mSystemControlManager = SystemControlManager.getInstance();
+
+        if (SystemProperties.getBoolean(PROP_MEMC, false) == true) {
+                memcStatus = MEMC_ON;
+        }else {
+                memcStatus = MEMC_OFF;
+        }
+        isT3Device = mSystemControlManager.hasMemcFunc();
+        mMEMCPref = (ListPreference) findPreference(KEY_MEMC);
+        mMEMCPref.setValueIndex(memcStatus);
+        mMEMCPref.setVisible(isT3Device);
+        mMEMCPref.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (TextUtils.equals(preference.getKey(), KEY_MEMC)) {
+            memcStatus = Integer.parseInt((String)newValue);
+            if (mSystemControlManager.hasMemcFunc()) {
+                if (memcStatus == MEMC_ON) {
+                    mSystemControlManager.memcContrl(true);
+                }else if (memcStatus == MEMC_OFF) {
+                    mSystemControlManager.memcContrl(false);
+                }
+            }
+        }
+        return true;
+    }
 }
